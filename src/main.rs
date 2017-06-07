baf9bb58f1f6325d77063e3b874838b52e3cc228
Norbytus <norbyt93@gmail.com>
2017-06-07 17:15:44 +0300

    Start add calp config

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate simple_excel_writer as excel;
 
 extern crate beanstalkd;
 
+extern crate clap;
+
 mod excel_msg;
 mod message;
 
@@ -19,7 +21,26 @@ use std::time::Duration;
 
 const MESSAGE: &'static str = r#"{"files":[{"file_name":"/tmp/test.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/tes2t.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/testsd.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/tessdadt.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]}]}"#;
 
+use clap::{Arg, App};
+
 fn main() {
+
+    let app = App::new("Excel queue worker")
+        .version("0.1")
+        .author("Alex")
+        .arg(Arg::with_name("Delay")
+             .short("d")
+             .long("delay")
+             .takes_value(true))
+        .arg(Arg::with_name("ip")
+             .short("i")
+             .long("ip")
+             .takes_value(true))
+        .arg(Arg::with_name("port")
+             .short("p")
+             .long("port")
+             .takes_value(true))
+        .get_matches();
 
     let mut queue = Beanstalkd::connect("0.0.0.0", 11300).unwrap();
 
