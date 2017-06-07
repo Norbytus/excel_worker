6cc91968c51b055c0b0798a9ecd44d76bc96a0ae
norbytus <norbyt93@gmail.com>
2017-06-08 00:38:51 +0300

    Add clap arguments and add daemonize

--- src/main.rs
+++ src/main.rs
@@ -10,45 +10,89 @@ extern crate beanstalkd;
 
 extern crate clap;
 
+extern crate daemonize;
+
 mod excel_msg;
 mod message;
 
 use message::Message;
 use beanstalkd::Beanstalkd;
+use daemonize::Daemonize;
 
 use std::thread;
 use std::time::Duration;
-
-const MESSAGE: &'static str = r#"{"files":[{"file_name":"/tmp/test.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/tes2t.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/testsd.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]},{"file_name":"/tmp/tessdadt.xlsx","sheets":[{"sheet_name":"t","fields":[["string",32,false],[false,32,"string"]]}]}]}"#;
 
 use clap::{Arg, App};
 
 fn main() {
 
-    let app = App::new("Excel queue worker")
+    let app = App::new("Worker for queue(Beanstalkd) create xlsx files from message and puck in zip file")
         .version("0.1")
-        .author("Alex")
-        .arg(Arg::with_name("Delay")
+        .author("Alexsander Startcev(Norbytus), norbyt93@gmail.com")
+        .arg(Arg::with_name("delay")
              .short("d")
              .long("delay")
+             .default_value("3")
              .takes_value(true))
         .arg(Arg::with_name("ip")
              .short("i")
              .long("ip")
+             .default_value("localhost")
              .takes_value(true))
         .arg(Arg::with_name("port")
              .short("p")
              .long("port")
+             .default_value("11300")
              .takes_value(true))
+        .arg(Arg::with_name("tube")
+             .short("t")
+             .long("tube")
+             .default_value("excel")
+             .takes_value(true))
+        .arg(Arg::with_name("daemon")
+             .long("daemon"))
         .get_matches();
 
-    let mut queue = Beanstalkd::connect("0.0.0.0", 11300).unwrap();
+    let delay = app.value_of("delay").unwrap().to_string();
+    let ip = app.value_of("ip").unwrap().to_string();
+    let port = app.value_of("port").unwrap().to_string();
+    let tube = app.value_of("tube").unwrap().to_string();
 
-    let _ = queue.watch("excel").unwrap();
+    if app.is_present("daemon") {
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/excel.pid")
+        .chown_pid_file(false)
+        .working_directory("/tmp")
+        .user("nobody")
+        .group("daemon")
+        .privileged_action(move || {
+            init(&delay, &ip, &port, &tube);
+            ()
+        });
+
+    match daemonize.start() {
+        Ok(_) => {},
+        Err(e) => println!("{:?}", e),
+    };
+    } else {
+        init(&delay, &ip, &port, &tube);
+    }
+
+
+}
+
+fn init(delay: &str, ip: &str, port: &str, tube: &str) {
+
+    use std::str::FromStr;
+
+    let mut queue = Beanstalkd::connect(ip, u16::from_str(port).unwrap())
+        .unwrap();
+
+    let _ = queue.watch(tube).unwrap();
 
     loop {
         match queue.reserve() {
-            Ok((id, message)) => {
+            Ok((_, message)) => {
                 let msg: Message = serde_json::from_str(&message).unwrap();
                 let xlsx_files = msg.get_files().unwrap();
                 xlsx_files.to_xlsx();
@@ -57,7 +101,7 @@ fn main() {
                 println!("{:?}", e);
             },
         }
-        thread::sleep(Duration::new(5, 0));
+        thread::sleep(Duration::new(u64::from_str(delay).unwrap_or(3), 0));
     }
 
 }
